b54ad35c7a8858d071a81a794e32328e16cc2d4f
YaroslavWork <yarik0980504744@gmail.com>
2026-01-04 22:27:51 +0100

    enhance graveyard drawing logic

--- scripts/game/board.py
+++ scripts/game/board.py
@@ -44,7 +44,7 @@ class Board:
         self.promoted_piece = None
 
         self._cBoard = chess.Board()
-        #self._cBoard.set_fen('7k/5Q2/6K1/8/8/8/8/8 w - - 0 1') # Checkmate or stalemate
+        self._cBoard.set_fen('7k/5Q2/6K1/8/8/8/8/8 w - - 0 1') # Checkmate or stalemate
         #self._cBoard.set_fen('8/8/8/8/8/2k5/2p5/2K5 w - - 0 1') # Insufficient material
 
         self.transform_sprite_sizes(size=self.square_size)
--- scripts/game/statistics.py
+++ scripts/game/statistics.py
@@ -42,29 +42,20 @@ class Statistics:
         self.white_backyard = board.white_graveyard
         self.black_backyard = board.black_graveyard
 
-        score = float(self.current_score) if self.current_score not in ['M', '-M'] else 0
-        if self.current_score == '+M':
-            score = 100
-        elif self.current_score == '-M':
-            score = -100
+        score = float(self.current_score) if self.current_score[1] != 'M' else 0
+        if self.current_score[0] == '+':
+            score = 10
+        elif self.current_score[0] == '-':
+            score = -10
         self.score_slider.update_score(score)
         self.score_slider.update_text(self.current_score)
 
     def draw(self, screen) -> None:
         self.draw_square_identifier(screen, (0, 0), 60)
         self.draw_score_information(screen)
+        self.draw_graveyards(screen, (0, 140), (self.size[0]-50, 50))
 
-        for i in range(len(self.white_backyard)):
-            piece_pos = (self.position[0] + i * 15, self.position[1] + 130)
-            screen.blit(
-                self.piece_sprite[self.white_backyard[i]], piece_pos
-            )
-
-        for i in range(len(self.black_backyard)):
-            piece_pos = (self.position[0] + i * 15, self.position[1] + 200)
-            screen.blit(
-                self.piece_sprite[self.black_backyard[i]], piece_pos
-            )
+        
 
     def draw_square_identifier(self, screen, position: pygame.Vector2, square_size: int) -> None:
         if self.current_square_position_str:
@@ -92,3 +83,18 @@ class Statistics:
         else:
             self.score_slider.set_loading(True)
         self.score_slider.draw(screen)
+
+    def draw_graveyards(self, screen, position: pygame.Vector2, size: pygame.Vector2) -> None:
+        relative_position = (self.position[0]+position[0], self.position[1]+position[1])
+        
+        for i in range(len(self.white_backyard)):
+            piece_pos = (relative_position[0] + i * 15, relative_position[1] / 2)
+            screen.blit(
+                self.piece_sprite[self.white_backyard[i]], piece_pos
+            )
+
+        for i in range(len(self.black_backyard)):
+            piece_pos = (relative_position[0] + size[0] - i * 15, relative_position[1] / 2)
+            screen.blit(
+                self.piece_sprite[self.black_backyard[i]], piece_pos
+            )
